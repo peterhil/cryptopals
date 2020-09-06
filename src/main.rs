2fe23b30b77817528712359176d4d1f4ac3e8403
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-09-06 03:45:57 +0300

    Add base64 crate to dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate base64;
+
+use base64::{encode, decode};
+
 fn main() {
-    println!("Hello, world!");
+    let a = b"hello world";
+    let b = "aGVsbG8gd29ybGQ=";
+
+    assert_eq!(encode(a), b);
+    assert_eq!(a, &decode(b).unwrap()[..]);
 }
