245a0397329adb58a8fc6832863cd71c72ad118e
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-10-04 18:21:44 +0300

    Use lazy_static to define the English language letter frequencies

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
 use data_encoding::{BASE64, HEXLOWER};
-use maplit::hashmap;
-use std::collections::HashMap;
 
 use cryptopals::ascii;
 use cryptopals::encoding;
@@ -42,34 +40,6 @@ fn ch3() {
     // - Only consider union of letters freqs in english and message (maybe normalise)
     // - Do Least squares on each letter
     // - Calculate median or mean
-    let _english: HashMap<char, f64> = hashmap!{
-        'a' => 0.08167,
-        'b' => 0.01492,
-        'c' => 0.02782,
-        'd' => 0.04253,
-        'e' => 0.12702,
-        'f' => 0.02228,
-        'g' => 0.02015,
-        'h' => 0.06094,
-        'i' => 0.06966,
-        'j' => 0.00153,
-        'k' => 0.00772,
-        'l' => 0.04025,
-        'm' => 0.02406,
-        'n' => 0.06749,
-        'o' => 0.07507,
-        'p' => 0.01929,
-        'q' => 0.00095,
-        'r' => 0.05987,
-        's' => 0.06327,
-        't' => 0.09056,
-        'u' => 0.02758,
-        'v' => 0.00978,
-        'w' => 0.0236,
-        'x' => 0.0015,
-        'y' => 0.01974,
-        'z' => 0.00074,
-    };
 
     // let decoded = &xor::xor_char(secret.to_vec(), b'z');
     // println!("Ch3: {:?}\n{:?}\n{:?}", &decoded, BASE64.encode(&decoded), ascii::print(decoded.to_vec()));
