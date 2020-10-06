74f30c7bbddddf3fd7c8e2de53cc116489526708
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-10-06 16:00:11 +0300

    Move metrics into separate function

--- src/main.rs
+++ src/main.rs
@@ -34,16 +34,13 @@ fn ch2() -> String {
     return v3;
 }
 
-fn ch3() {
-    let hex = b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-    let secret: Vec<u8> = encoding::hex_decode(hex.to_vec());
-    let letters: Vec<u8> = (0..=255).collect::<Vec<u8>>();
-    let mut metrics: BTreeMap<OrderedFloat<f64>, BTreeMap<char, Vec<u8>>> = BTreeMap::new();
+pub fn metrics(secret: &Vec<u8>, letters: &Vec<u8>) -> BTreeMap<OrderedFloat<f64>, BTreeMap<char, Vec<u8>>> {
+    let mut metrics = BTreeMap::new();
 
     letters
         .iter()
         .for_each(|&letter| {
-            let decoded = &xor::xor_char(&secret, letter);
+            let decoded = &xor::xor_char(secret, letter);
 
             // Convert to string, lowercase the message and calculate metric
             match String::from_utf8(decoded.to_vec()) {
@@ -57,6 +54,15 @@ fn ch3() {
                 Err(_) => (),
             }
         });
+
+    return metrics;
+}
+
+fn ch3() {
+    let hex = b"1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let secret: Vec<u8> = encoding::hex_decode(hex.to_vec());
+    let letters: Vec<u8> = (0..=255).collect::<Vec<u8>>();
+    let metrics = metrics(&secret, &letters);
 
     // Get minimum element of BTreeMap: https://stackoverflow.com/a/58951038/470560
     if let Some((minimum, decodings)) = metrics.iter().next_back() {
