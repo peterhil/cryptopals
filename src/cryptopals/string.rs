45c137f78463438189195198ee6132f2b4943bbc
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-10-06 13:51:52 +0300

    Fix string::from_vec to return empty string on error

--- src/cryptopals/string.rs
+++ src/cryptopals/string.rs
@@ -3,6 +3,6 @@
 pub fn from_vec(buf: Vec<u8>) -> String {
     return match String::from_utf8(buf) {
         Ok(v) => v,
-        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        Err(e) => "".to_string(), //format!("Error: {}", e), //panic!("Invalid UTF-8 sequence: {}", e),
     };
 }
