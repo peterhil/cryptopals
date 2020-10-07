0e64d82e6be71bcf0dd1a72f1790dba4ae90d34b
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-10-07 16:15:43 +0300

    s1c4: Fix the return type

--- src/bin/set1/ch4.rs
+++ src/bin/set1/ch4.rs
@@ -1,16 +1,15 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
-use relative_path::RelativePath;
 use std::env;
 
 use cryptopals::encoding;
-use cryptopals::io::{read_lines};
+use cryptopals::io::{exit_err, read_lines};
 use cryptopals::xor;
 
-fn ch4() -> Result<String, std::io::Error> {
-    // File must exist in current path before this produces output
-    let relative_path = RelativePath::new("./data/4.txt");
-    let full_path = relative_path.to_path(env::current_dir()?.as_path());
+fn ch4() {
+    let full_path = env::args().nth(1)
+        .ok_or(format!("Usage: {} data/4.txt", env::args().nth(0).unwrap()))
+        .unwrap_or_else(|e| exit_err(e, 1));
 
     match read_lines(full_path) {
         Ok(lines) => {
@@ -27,12 +26,9 @@ fn ch4() -> Result<String, std::io::Error> {
             println!("Error: {:?}", err);
         },
     }
-
-    return Ok("Done!".to_string());
 }
 
 fn main() {
     println!("Ch4:");
-    let res = ch4();
-    println!("{}", res.unwrap())  // TODO Fix type of ch4;
+    ch4();
 }
