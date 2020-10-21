6f40a65fb9c1f4f6654ca02d1965ae6b6e804811
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-10-21 09:47:46 +0300

    Show spaces on ascii::printable

--- src/cryptopals/ascii.rs
+++ src/cryptopals/ascii.rs
@@ -1,18 +1,18 @@
 #![allow(unused_variables)]
 
 pub fn printable(byte: char) -> char {
-    if char::is_ascii_graphic(&byte) {
-        return byte;
-    } else {
+    if char::is_ascii_control(&byte) {
         return '.';
+    } else {
+        return byte;
     }
 }
 
 pub fn printable_escape(byte: char) -> String {
-    if char::is_ascii_graphic(&byte) {
-        return byte.to_string();
-    } else {
+    if char::is_ascii_control(&byte) {
         return format!("\\{{{}}}", byte as u8);
+    } else {
+        return byte.to_string();
     }
 }
 
