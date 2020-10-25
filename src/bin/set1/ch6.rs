974afd72461200abd6af5ccdc0d2659c69ab06b6
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-10-25 14:10:03 +0200

    s1c6: Try to guess the repeating key by using transpose

--- src/bin/set1/ch6.rs
+++ src/bin/set1/ch6.rs
@@ -1,14 +1,15 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
-use data_encoding::BASE64;
+use data_encoding::{BASE64, HEXLOWER};
 use ordered_float::OrderedFloat;
 use std::collections::{BTreeMap, BTreeSet};
 use std::env;
+use transpose::transpose;
 
 use cryptopals::ascii;
 use cryptopals::io::{exit_err, read_lines};
 use cryptopals::stat::text;
-// use cryptopals::xor;
+use cryptopals::xor;
 
 fn edit_distance_metric(s1: Vec<u8>, s2: Vec<u8>) -> f64 {
     // assert_eq!(s1.len(), s2.len(), "Arguments must be the same length!");
@@ -73,10 +74,7 @@ fn guess_keysize(secret: &Vec<u8>, max_keysize: usize) ->
     return metrics;
 }
 
-fn read_bytes() -> Vec<u8> {
-    let full_path = env::args().nth(1)
-        .ok_or(format!("Usage: {} data/6.txt", env::args().nth(0).unwrap()))
-        .unwrap_or_else(|e| exit_err(e, 1));
+fn read_bytes(full_path: String) -> Vec<u8> {
     let mut contents = vec![];
 
     match read_lines(full_path) {
@@ -86,34 +84,82 @@ fn read_bytes() -> Vec<u8> {
                 if let Ok(base64) = line {
                     let mut decoded = BASE64
                         .decode(&base64.as_bytes()[..])
-                        .unwrap_or_else(|e| exit_err(e, 2));
+                        .unwrap_or_else(|e| exit_err(e, 3));
                     contents.append(&mut decoded);
                 }
             }
         },
         Err(err) => {
-            println!("Error: {:?}", err);
+            exit_err(err, 2);
         },
     }
 
     return contents;
 }
 
-fn ch6(secret: Vec<u8>) {
-    let metrics = guess_keysize(&secret, 40);
+fn get_secret() -> Vec<u8> {
+    let plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+    let key = "ICE";
+    let secret: Vec<u8> = xor::encrypt_repeated(&plaintext.as_bytes().to_vec(), &key.as_bytes().to_vec());
+    // let secret: Vec<u8> = BASE64.decode(b"HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS").unwrap();
+    // let secret: Vec<u8> = b"ICEICE Burning 'em, if you ain't quick and nimble".to_vec();
+
+    // let full_path = env::args().nth(1)
+    //     .ok_or(format!("Usage: {} data/6.txt", env::args().nth(0).unwrap()))
+    //     .unwrap_or_else(|e| exit_err(e, 1));
+    // let secret = read_bytes(full_path);
+
+    return secret;
+}
+
+fn get_keysize(secret: &Vec<u8>) -> usize {
+    let metrics = guess_keysize(&secret, 15);
+    let mut keysize: usize = 0;
+
     if let Some((metric, sizes)) = metrics.iter().next() {
         println!("Probable keysizes: {:?} with metric {:?}", sizes, metric.into_inner());
+        keysize = *sizes.iter().nth(0).unwrap();
     }
+
+    return keysize;
+}
+
+fn pick_repeating_key(secret: &Vec<u8>) -> Vec<u8> {
+    let keysize = 3;
+    // let byte_count = keysize * (keysize * 14);
+    // let keysize = get_keysize(&secret);
+    let byte_count = keysize * (secret.len() / keysize);
+
+    let blocksize = byte_count / keysize;
+    println!("Using keysize: {:?}, and block size: {:?} for {:?} bytes\n", keysize, blocksize, byte_count);
+
+    let m: Vec<u8> = secret.iter().cloned().take(byte_count).collect::<Vec<u8>>();
+    let mut blocks = vec![0; byte_count];
+
+    transpose(&m, &mut blocks, keysize, blocksize);
+    println!("Bytes: {:02x?}", &m);
+    println!("Trans: {:02x?}\n", &blocks);
+
+    let mut key: Vec<u8> = vec![];
+
+    for block in blocks.chunks(blocksize) {
+        println!("Block: {:02x?}", &block);
+        if let Some(ch) = xor::decrypt_single_byte(&block.to_vec()) {
+            key.push(ch);
+        }
+    }
+
+    return key;
 }
 
 fn main() {
-    // let plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
-    // let key = "ICE";
-    // let secret: Vec<u8> = xor::encrypt_repeated(plaintext, key);
-    // let secret: Vec<u8> = BASE64.decode(b"HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS").unwrap();
-    // let secret: Vec<u8> = b"ICEICE Burning 'em, if you ain't quick and nimble".to_vec();
-    let secret = read_bytes();
+    let secret: Vec<u8> = get_secret();
 
     println!("Ch6:");
-    ch6(secret);
+    println!("Secret length: {:?}", &secret.len());
+
+    let key: Vec<u8> = pick_repeating_key(&secret);
+    let decrypted = xor::encrypt_repeated(&secret, &key);
+    println!("{:?}", ascii::print(decrypted));
+    println!("KEY: {:?}", String::from_utf8(key).unwrap());
 }
