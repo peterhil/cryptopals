b501cafa3ceca0ca96dace8c0ae5996bdc61b1c9
Peter Hillerström <peter.hillerstrom@gmail.com>
2020-11-01 14:27:17 +0200

    s1c4: Use BufReader directly

--- src/bin/set1/ch4.rs
+++ src/bin/set1/ch4.rs
@@ -2,32 +2,39 @@
 
 use data_encoding::HEXLOWER;
 use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
-use cryptopals::io::{exit_err, read_lines};
+use cryptopals::io::{exit_err};
 use cryptopals::xor;
 
-fn ch4() {
+
+fn ch4() -> io::Result<()> {
     let full_path = env::args().nth(1)
         .ok_or(format!("Usage: {} data/4.txt", env::args().nth(0).unwrap()))
         .unwrap_or_else(|e| exit_err(e, 1));
+    let file = File::open(full_path)?;
+    let reader = BufReader::new(file);
 
-    match read_lines(full_path) {
-        Ok(lines) => {
-            // Consumes the iterator, returns an (Optional) String
-            for line in lines {
-                if let Ok(hex) = line {
-                    let secret = HEXLOWER.decode(hex.as_bytes()).unwrap();
-                    xor::decrypt_single_byte(&secret, 1);
-                }
-            }
-        },
-        Err(err) => {
-            exit_err(err, 2);
-        },
+    for line in reader.lines() {
+        if let Ok(hex) = line {
+            let secret = HEXLOWER.decode(hex.as_bytes()).unwrap();
+            xor::decrypt_single_byte(&secret, 1);
+        }
     }
+
+    Ok(())
 }
+
 
 fn main() {
     println!("Ch4:");
-    ch4();
+    match ch4() {
+        Ok(res) => {
+            res
+        },
+        Err(err) => {
+            exit_err(err, 2)
+        }
+    }
 }
